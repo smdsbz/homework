32cd25f6af310343967bd223702b6df271db35dd
smdsbz <694066681@qq.com>
2017-01-15 21:28:53 +0800

    working on personal data update

--- HR-building/main.py
+++ HR-building/main.py
@@ -19,7 +19,7 @@ def getConditonal(column, conditon, require):
 	database = sqlite3.connect(os.path.join(app.root_path, 'data.db'))
 	cursor = database.execute("select %s from test where %s = '%s'" % (column, conditon, require))
 	data = cursor.fetchall()
-	print(data)
+	#print(data)
 	return data
 
 def verify(id, passwd):
@@ -55,6 +55,8 @@ def login():
 		try:
 			if verify(session['id'], session['passwd']):
 				return redirect(url_for('personal'))
+		except ValueError:
+			return redirect(url_for('update'))
 		except TypeError:
 			flash("用户名或密码错误，请仔细核对！")
 			return render_template('login.html')
@@ -64,7 +66,7 @@ def personal():
 	try:
 		return render_template('personal_base.html', database = getConditonal('*','id',session['id']))
 	except KeyError:
-		flash("请登录再进行操作！")
+		flash("请登录后再进行操作！")
 		return redirect(url_for('index'))
 
 @app.route('/logout/')
@@ -73,10 +75,11 @@ def logout():
 		session.pop('id', None)
 	return redirect(url_for('index'))
 
-'''
-@app.route('/edit_profile/', methods=['GET', 'POST'])
+@app.route('/update/', methods=['GET', 'POST'])
 def update():
-'''
+	if request.method == 'GET':
+		return render_template('info_update.html', database=getConditonal('*','id',session['id']))
+
 
 ######## main ########
 
