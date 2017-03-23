6a406a5ffa47ede9a91087a90085cedfc673dac4
smdsbz <694066681@qq.com>
2017-03-23 21:38:40 +0800

    .

--- C/EXPERIMENT.c
+++ C/EXPERIMENT.c
@@ -1,10 +1,26 @@
-#include<stdio.h>
-void main( )
-{
-    int a, b, t;
-    printf("Input two integers:");
-    scanf("%d %d",&a,&b);
-	b += a, a = b - a, b -= a;
-    printf("\na=%d,b=%d",a,b);
-}
+#include "stdio.h"
 
+void main(void) {
+    puts("Here's a third: ");
+    float pocket = 1./3;
+    printf("%f\n", pocket);
+
+
+    puts("\nnow times it with 7 and then subtracts it with 2 like craaaaazy!");
+    puts("(Note: in analog env, a third will still be a third)");
+
+    unsigned long times_to_repeat = -1;
+    printf("\nHow many times should I repeat this operation? ");
+    scanf("%ld", &times_to_repeat);
+
+
+    // start!
+    for (; times_to_repeat != 0; --times_to_repeat) {
+    	pocket *= 7;
+    	pocket -= 2;
+        printf("Clerk: Here's your bill: %f\n", pocket);
+    }
+    
+    
+
+}
