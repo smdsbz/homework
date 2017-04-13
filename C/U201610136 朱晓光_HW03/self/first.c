e4032dcff47b18a54858caa4db6882b8320ca5a3
smdsbz <694066681@qq.com>
2017-04-13 23:39:46 +0800

    .

--- C/U201610136 朱晓光_HW03/self/first.c
+++ C/U201610136 朱晓光_HW03/self/first.c
@@ -1,13 +1,8 @@
 #include "stdio.h"
-
-
-#define TARGET      24
-
-
+#include "second.h"
+int * copy(int *);
 void main(void) {
-
-    
-
-
-
+    int array[5] = {1, 2, 3, 4, 5};
+    printf("%p -> %d\n", &array[2], array[2]);
+    printf("%p -> %d\n", &copy(&array[1])[2], copy(&array[1])[2]);
 }
---
+++ C/U201610136 朱晓光_HW03/self/second.c
@@ -0,0 +1,4 @@
+#include "stdio.h"
+int * copy(int * name) {
+    return name;
+}
