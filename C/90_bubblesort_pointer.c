8a7666081cd651e19a4555fb50e6d9db5468c925
smdsbz <694066681@qq.com>
2017-05-05 21:10:40 +0800

    .

--- C/90_bubblesort_pointer.c
+++ C/90_bubblesort_pointer.c
@@ -17,20 +17,40 @@ int getNumbers(int *nums) {
 }
 
 
+void putNumbers(int *nums) {
+    unsigned i = 0;
+    for (; i < NUM_CNT; ++i) {
+        printf("%d", nums[i]);
+        if (i == NUM_CNT-1) { putchar('\n'); }
+        else { putchar('-'); putchar('>'); }
+    }
+}
 
+
+
+
+/*
 void swapNumbers(int *a, int *b) {
     *b += *a;
     *a = *b - *a;
     *b -= *a;
     return;
 }
-
+*/
 
 void bubbleSort(int *nums) {
 
     int *outer = nums, *inner = nums;
 
-    for (; outer-nums > )
+    for (; outer-nums < NUM_CNT-1; ++outer) {
+        for (inner = nums; inner-nums < NUM_CNT-(outer-nums)-1; ++inner) {
+            if (*inner > *(inner+1)) {
+                *(inner+1) += *inner;
+                *inner = *(inner+1) - *inner;
+                *(inner+1) -= *inner;
+            }
+        }
+    }
 
 
 }
@@ -45,9 +65,9 @@ int main(int argc, char const *argv[]) {
 
     while (getNumbers(nums)) {
 
+        bubbleSort(nums);
 
-
-
+        putNumbers(nums);
 
     }
 
