f210cd527b82698de440a61bda76f2ae980df927
smdsbz <694066681@qq.com>
2017-05-11 18:54:05 +0800

    .

---
+++ C/U201610136 朱晓光_HW06/report/debug.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+char *strcpy(char *,char *);
+void main(void)
+{
+    char a[20],b[60]="there is a boat on the lake.";
+    printf("%s\n",strcpy(a,b));
+
+}
+char *strcpy(char *s,char *t)
+{
+    while(*s++=*t++)
+    ;
+    return (s);
+}
