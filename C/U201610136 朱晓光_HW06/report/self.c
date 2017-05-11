ca7d817cd2c9ee6aa889ae92687e547b1850d154
smdsbz <694066681@qq.com>
2017-05-11 15:13:22 +0800

    .

---
+++ C/U201610136 朱晓光_HW06/report/self.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//////////////////////////////////////////////////
+/*****  Class Definition  *****/
+/***    Class Members      ***/
+typedef struct MyStruct {
+    int     _some_number;
+    void    (*f)(struct MyStruct *);
+    struct MyStruct *   self;
+} MyClass;
+/***    Class Functions    ***/
+MyClass __init_MyClass__(
+    int a, void (*func)(MyClass *)
+) {
+    /*  Class Init Function
+        Returns the desired class object
+    */
+    MyClass obj;
+    obj._some_number = a;
+    obj.f = func;
+    return obj;
+}
+void func(MyClass *self) {
+    /* Some Other Function */
+    scanf("%d", &(self->_some_number));
+    puts("Hello, Wolrd!");
+    printf("self._some_number = %d\n", self->_some_number);
+    return;
+}
+///////////////////////////////////////////////////
+void main(void) {
+    /* Instantiating Class Object */
+    MyClass Object = __init_MyClass__(42, func);
+    Object.self = &Object;
+    // Instantiation Finished
+    Object.f(&Object);
+}
