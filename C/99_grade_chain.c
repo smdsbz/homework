5030ae896a49940990961c34d3a86a9fca1f74b9
smdsbz <694066681@qq.com>
2017-05-20 19:01:22 +0800

    .

--- C/99_grade_chain.c
+++ C/99_grade_chain.c
@@ -13,6 +13,14 @@ enum Courses {
     C
 };
 
+int getCourseCode(char const *name) {
+    if (!strcmp(name, "English")) { return 0; }
+    else if (!strcmp(name, "Math")) { return 1; }
+    else if (!strcmp(name, "Physics")) { return 2; }
+    else if (!strcmp(name, "C")) { return 3; }
+    else { return -1; }
+}
+
 #define COURSE_CNT  4
 
 //////////////////////////////////
@@ -21,8 +29,9 @@ enum Courses {
 
 typedef struct _Student {
     char    id[15];
-    int     name[20];
+    char     name[20];
     float   grades[COURSE_CNT];
+    double  avg;
 } Student;
 
 
@@ -49,16 +58,38 @@ void _printStudent(Student stu) {
            stu.id, stu.name,
            stu.grades[English],
            stu.grades[Math],
-           stu.grades[Pysics],
+           stu.grades[Physics],
            stu.grades[C]);
     return;
 }
 
 
-// TODO: getStudentById(...) needed
-// void modifyStudent(void) {
-//
-// }
+void _printStudentStat(Student *stu) {
+    /*  ID NAME SUM AVG
+    */
+    double sum = 0;
+    for (int i = 0; i < COURSE_CNT; ++i) {
+        sum += stu->grades[i];
+    }
+    stu->avg = sum / COURSE_CNT;
+    printf("%-15s%-20s%-10.2f%-10.2f\n",
+           stu->id, stu->name,
+           sum, (sum / COURSE_CNT));
+    return;
+}
+
+
+double _compareStudent(Student Alice, Student Bob) {
+    return (Alice.avg - Bob.avg);
+}
+
+
+void _modifyStudent(Student *stu,
+                    char const *course_name,
+                    float new_score) {
+    stu->grades[getCourseCode(course_name)] = new_score;
+    return;
+}
 
 
 
@@ -94,8 +125,8 @@ Node * initStudents(int cnt) {
         subs->next = NULL;
         subs->data = _initStudent();    // toggle data input
     }
+    return head;
 }
-
 
 
 void printStudents(Node *cur) {
@@ -103,28 +134,136 @@ void printStudents(Node *cur) {
     */
     printf("%-15s%-20s%-10s%-10s%-10s%-10s\n",
            "ID", "Name", "English", "Math", "Physics", "C");
-    for (; True; cur = cur->next) {
+    for (; cur; cur = cur->next) {
         _printStudent(*(cur->data));
-        if (cur->next == NULL) { break; }
+        // if (cur->next == NULL) { break; }
     }
     putchar('\n');
 }
 
 
+void printStudentsStat(Node *head) {
+    printf("%-15s%-20s%-10s%-10s\n",
+           "ID", "Name", "SUM", "AVG");
+    for (; head; head = head->next) {
+        _printStudentStat(head->data);
+    }
+    putchar('\n');
+    return;
+}
 
-Student * getStudentById(char const *id) {
 
+void printStudentsAvg(Node *head) {
+    printf("%-15s%-20s%-10s\n",
+           "ID", "Name", "AVG");
+    for (; head; head = head->next) {
+        printf("%-15s%-20s%-10.2f\n",
+               head->data->id,
+               head->data->name,
+               head->data->avg);
+        // pass
+    }
+    putchar('\n');
+    return;
+}
+
+
+Node * getNodeById(Node *head, char const *id) {
+    Node *n = head;
+    for (; n; n = n->next) {
+        if (!strcmp(id, n->data->id)) { break; }    // node found
+        if (n->next == NULL) { n = NULL; break; }   // end of chain
+    }
+    return n;
+}
+
+
+void modifyChain(Node *head) {
+    /*  given the head of the chain
+        will toggle input action
+    */
+    char id[15]; char course_name[20]; float new_score;
+    scanf("%s %s %f", id, course_name, &new_score); getchar();
+    _modifyStudent(getNodeById(head, id)->data,
+                   course_name,
+                   new_score);
+    return;
+}
+
+
+double compareNode(Node A, Node B) {
+    return _compareStudent(*(A.data), *(B.data));
+}
+
+
+void sortChainSwapData(Node *prev, int len) {
+    int i, j;
+    Node *head = prev;
+    Node *subs;
+    for (i = 0; i < len-1; ++i, prev = prev->next) {
+        for (j = 0, subs = head; j < len-i-1; ++j, subs = subs->next) {
+            if (compareNode(*subs, *(subs->next)) > 0) {
+                Student *tmp = subs->data;   // ye only have to swap the data pointer
+                subs->data = subs->next->data;
+                subs->next->data = tmp;
+                // puts("===================");
+                // printStudentsAvg(head);
+                // puts("===================");
+            }
+        }
+    }
+    return;
+}
+
+
+// void sortChainChangeRoute(...);
+
+
+void freeStudents(Node *cur) {
+    Node *pnode;
+    while (cur) {
+        pnode = cur;
+        cur = cur->next;
+        _freeStudent(pnode->data);    // free data storage
+        free(pnode);                  // free Node
+    }
+    return;
 }
 
 
 
+///////////////////////////////////////////////////////////////////////
 
-void freeStudents(Node *cur) {
-    while (True) {
-        _freeStudent(cur->data);    // free data storage
-        free(cur);                  // free Node
-        if (cur->next != NULL) { cur = cur->next; }
-        else { break; }
+
+void main(void) {
+
+    int cnt;
+    scanf("%d", &cnt); getchar();
+
+
+    Node *chain = initStudents(cnt);
+
+    printStudents(chain);
+
+    int change_cnt;
+    scanf("%d", &change_cnt); getchar();
+
+    for (; change_cnt; --change_cnt) {
+        modifyChain(chain);
     }
+
+    puts("Alter:");
+    printStudents(chain);
+
+    puts("SumAndAvg:");
+    printStudentsStat(chain);
+
+    puts("Sort:");
+    sortChainSwapData(chain, cnt);
+    printStudentsAvg(chain);
+
+
+    freeStudents(chain);
+
     return;
 }
---
+++ C/99_grade_chain_API.md
@@ -0,0 +1,61 @@
+# 99_grade_chain.c
+
+-------------------------------------
+
+## API
+
+### Courses
+
+Course      |   Code
+------------|------------
+English     |   0
+Math        |   1
+Pysics      |   2
+C           |   3
+
+```C
+#define COURSE_CNT      4
+int getCourseCode(char const *name);
+```
+-------------------------------------
+
+### Student Class
+
+```C
+typedef struct _Student {
+    char    id[15];
+    int     name[20];
+    float   grades[COURSE_CNT];
+    double  avg;
+} Student;
+
+Student * _initStudent(void);
+void _printStudent(Student stu);
+void _printStudentStat(Student *stu);
+void _modifyStudent(Student *stu,
+                    char const *course_name,
+                    float new_score);
+double _compareStudent(Student A, Student B);
+void _freeStudent(Student *stu);
+```
+-------------------------------------
+
+### Node Class
+_wrapping the Student Class_
+
+```C
+typedef struct _Node {
+    Student         *data;
+    struct _Node    *next;
+} Node;
+
+Node * initStudents(int cnt);
+void printStudents(Node *cur);
+void printStudentsStat(Node *head);
+void printStudentsAvg(Node *head);
+Node * getNodeById(Node *head, char const *id);
+void modifyChain(Node *head);
+double compareNode(Node A, Node B);
+void sortChainSwapData(Node *head, int cnt);
+void freeStudents(Node *cur);
+```
