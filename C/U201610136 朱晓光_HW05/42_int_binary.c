7ede0af7e4726d199df65699eb8f8e3a790360f6
smdsbz <694066681@qq.com>
2017-06-07 07:44:16 +0800

    .

--- C/U201610136 朱晓光_HW05/42_int_binary.c
+++ C/U201610136 朱晓光_HW05/42_int_binary.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-void printBits(int);
+void printBits(int, char *);
 void main(void) {
     unsigned N;
     scanf("%u", &N); getchar();
     int nums[120] = {0};
+    char strNums[sizeof(int)*8+1] = {'\0'};
     unsigned i;
     for (i = 0; i < N; ++i) {
         scanf("%d", (nums + i));
     }
     for (i = 0; i < N; ++i) {
-        printBits(nums[i]);
+        printBits(nums[i], strNums);
     }
 }
-void printBits(int dec) {
+void printBits(int dec, char *str) {
     int cur;
     for (cur = sizeof(int)*8-1; cur >= 0; --cur) {
-        putchar( ( dec & (1<<cur) ) ? '1' : '0' );
+        str[sizeof(int)*8-1-cur] = ( ( dec & (1<<cur) ) ? '1' : '0' );
     }
-    putchar('\n');
+    str[sizeof(int)*8] = 0;
+    printf("%s\n", str);
 }
